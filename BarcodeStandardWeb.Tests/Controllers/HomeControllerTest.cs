5766fd18e29267c78515e462277d635f1dba62a9
Mark Hazleton <mark.hazleton@controlorigins.com>
2021-03-29 19:41:51 -0500

    clean-up

--- BarcodeStandardWeb.Tests/Controllers/HomeControllerTest.cs
+++ BarcodeStandardWeb.Tests/Controllers/HomeControllerTest.cs
@@ -8,13 +8,13 @@ using BarcodeStandardWeb;
 using BarcodeStandardWeb.Controllers;
 
 namespace BarcodeStandardWeb.Tests.Controllers
-{
+    {
     [TestClass]
     public class HomeControllerTest
-    {
+        {
         [TestMethod]
         public void Index()
-        {
+            {
             // Arrange
             HomeController controller = new HomeController();
 
@@ -23,32 +23,6 @@ namespace BarcodeStandardWeb.Tests.Controllers
 
             // Assert
             Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
+            }
         }
     }
-}
--- BarcodeStandardWeb/Controllers/HomeController.cs
+++ BarcodeStandardWeb/Controllers/HomeController.cs
@@ -8,24 +8,10 @@
     using System.Web.Mvc;
     public class HomeController : BaseController
     {
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
         public ActionResult Index()
         {
             return View();
         }
-
 
         [AcceptVerbs(HttpVerbs.Get)]
         public ActionResult GetFile()
